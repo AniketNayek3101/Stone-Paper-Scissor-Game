d46138a72a33cf535a0066ef9fe7a3db91488e12
Aniket Nayek <nayekaniket2017@gmail.com>
2022-01-01 17:21:09 +0530

    Second commit / working structure

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 function game(user){
-
+    const combo = user + compchoice;
+    if(combo == "pr" || combo == "rs" || combo == "sp"){
+        document.querySelector('.winn').innerText = "You won!!!";
+    }
+    else if (combo == "rp" || combo == "sr" || combo == "ps"){
+    document.querySelector('.winn').innerText = "Computer won!!!";
+    }
+    else {
+    document.querySelector('.winn').innerText = "Its a tie";
+    }
 }
 
-stone.addEventListener('click',function(){
+const stone = document.getElementsByClassName('stone');
+const paper = document.getElementsByClassName('paper');
+const scissor = document.getElementsByClassName('scissor');
+
+stone[0].addEventListener('click',function(){
     game('r')
+    /*console.log("Stone")*/;
 })
-paper.addEventListener('click',function(){
+paper[0].addEventListener('click',function(){
     game('p')
+        /*console.log("paper")*/;
+
 })
-scissor.addEventListener('click',function(){
+scissor[0].addEventListener('click',function(){
     game('s')
+        /*console.log("Scissor")*/;
+
 })
+
+const comp = ['r','p','s'];
+const choice = Math.floor(Math.random() * 3);
+const compchoice = comp[choice];
+/*console.log(compchoice)*/;
+
 
