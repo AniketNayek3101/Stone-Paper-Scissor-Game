f7bbbe6c841e0677c77a5b5a26c74d741131d6c9
Aniket Nayek <nayekaniket2017@gmail.com>
2022-01-01 16:11:55 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function game(user){
+
+}
+
+stone.addEventListener('click',function(){
+    game('r')
+})
+paper.addEventListener('click',function(){
+    game('p')
+})
+scissor.addEventListener('click',function(){
+    game('s')
+})
+
