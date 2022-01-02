42aec301d19c3ba730136fc0d1651c2fecc02f6b
Aniket Nayek <nayekaniket2017@gmail.com>
2022-01-02 15:07:25 +0530

    Adding play again

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-function game(user){
+function game(user) {
     const combo = user + compchoice;
-    if(combo == "pr" || combo == "rs" || combo == "sp"){
+    if (combo == "pr" || combo == "rs" || combo == "sp") {
         document.querySelector('.winn').innerText = "You won!!!";
     }
-    else if (combo == "rp" || combo == "sr" || combo == "ps"){
-    document.querySelector('.winn').innerText = "Computer won!!!";
+    else if (combo == "rp" || combo == "sr" || combo == "ps") {
+        document.querySelector('.winn').innerText = "Computer won!!!";
     }
     else {
-    document.querySelector('.winn').innerText = "It's a tie";
+        document.querySelector('.winn').innerText = "It's a tie";
     }
 }
 
@@ -15,25 +15,27 @@ function game(user){
 const stone = document.getElementsByClassName('stone');
 const paper = document.getElementsByClassName('paper');
 const scissor = document.getElementsByClassName('scissor');
+const button = document.getElementsByClassName('btn');
 
-stone[0].addEventListener('click',function(){
+stone[0].addEventListener('click', function () {
     game('r')
     /*console.log("Stone")*/;
 })
-paper[0].addEventListener('click',function(){
+paper[0].addEventListener('click', function () {
     game('p')
         /*console.log("paper")*/;
 
 })
-scissor[0].addEventListener('click',function(){
+scissor[0].addEventListener('click', function () {
     game('s')
         /*console.log("Scissor")*/;
 
 })
 
-const comp = ['r','p','s'];
+button[0].addEventListener('click', function () {
+    window.location.reload();
+})
+const comp = ['r', 'p', 's'];
 const choice = Math.floor(Math.random() * 3);
 const compchoice = comp[choice];
 /*console.log(compchoice)*/;
-
-
